fa674571a7a99a6d970bbaa204a3fdd29a7b4884
Laurent Lanteigne <lanteignel@uchicago.edu>
2025-02-18 22:41:29 -0600

    Created class for the different classifiers and clusters, next is to build the walk-forward backtester.

--- src/vxx_trade/backtester.py
+++ src/vxx_trade/backtester.py
@@ -5,12 +5,19 @@ from enum import Enum
 from vxx_trade.data_generator import generate_data_for_strategy
 from sklearn.cluster import KMeans, AgglomerativeClustering
 from sklearn.mixture import GaussianMixture
+from sklearn.ensemble import RandomForestClassifier
+from skopt import BayesSearchCV
+from skopt.space import Real, Categorical, Integer
+from xgboost import XGBClassifier
+import polars as pl 
+
 
 START_EVAL_DATE = datetime.date(2016, 1, 1)
 RANDOM_STATE = 42
 
 class ClusteringAlgorithm(ABC):
     def __init__(self, n_clusters: int, name: str):
+        self.model = None
         self.n_clusters = n_clusters
         self.name = name
 
@@ -23,10 +30,13 @@ class ClusteringAlgorithm(ABC):
     def predict(self, df):
         pass
 
+    def __repr__(self):
+        return f"{self.name}(n_clusters={self.n_clusters})"
+
 
 class KMeansClustering(ClusteringAlgorithm):
     def __init__(self, n_clusters: int):
-        super().__init__(n_clusters, "KMeans")
+        super().__init__(n_clusters, "KMeansCluster")
         self.model = KMeans(n_clusters=n_clusters, random_state=RANDOM_STATE)
 
     def fit(self, df):
@@ -36,12 +46,128 @@ class KMeansClustering(ClusteringAlgorithm):
         return self.model.predict(df.to_numpy())
 
 
+class HierarchicalClustering(ClusteringAlgorithm):
+    def __init__(self, n_clusters: int):
+        super().__init__(n_clusters, "HierarchicalCluster")
+        self.model = AgglomerativeClustering(n_clusters=n_clusters)
+
+    def fit(self, df):
+        self.model.fit(df.to_numpy())
+
+    def predict(self, df):
+        return self.model.fit_predict(df.to_numpy())
+
+
+class GMMClustering(ClusteringAlgorithm):
+    def __init__(self, n_clusters: int):
+        super().__init__(n_clusters, "GMMCluster")
+        self.model = GaussianMixture(n_components=n_clusters, random_state=RANDOM_STATE)
+
+    def fit(self, df):
+        self.model.fit(df.to_numpy())
+
+    def predict(self, df):
+        return self.model.predict(df.to_numpy())
+
 
 class EvalFrequency(Enum):
     MONTHLY = "MONTHLY"
     QUARTERLY = "QUARTERLY"
     SEMI_ANNUALLY = "SEMI_ANNUALLY"
     ANNUALLY = "ANNUALLY"
+
+
+class ClassifierModel(ABC):
+    @abstractmethod
+    def fit(self, train, target):
+        pass
+
+    @abstractmethod
+    def predict(self, test):
+        pass
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}"
+
+
+class RandomForestClassifierSimple(ClassifierModel):
+    def __init__(self, n_estimators: int = 100, random_state: int = RANDOM_STATE):
+        self.n_estimators = n_estimators
+        self.random_state = random_state
+        self.model = RandomForestClassifier(n_estimators=self.n_estimators, random_state=self.random_state)
+
+
+    def fit(self, train, target) -> None:
+        self.model.fit(train, target)
+
+    def predict(self, test):
+        return self.model.predict(test)
+
+    def __repr__(self) -> str:
+        return f"RandomForestClassifier(n_estimators={self.n_estimators}, random_state={self.random_state})"
+
+
+class RandomForestClassifierCV(ClassifierModel):
+    def __init__(self, cv: int = 5, random_state: int = RANDOM_STATE, n_iter: int = 25, scoring: str = 'accuracy', verbose: bool = True):
+        self.params = {
+            'n_estimators': Integer(50, 1000),
+            'max_depth': Integer(3, 10),
+            'min_samples_split': Integer(2, 10),
+            'min_samples_leaf': Real(1e-2, 1e-1, prior='log-uniform'),
+            'criterion': Categorical(['gini', 'entropy', 'log_loss']),
+            'max_features': Categorical(['log2', 'sqrt'])
+        }
+        self.cv = cv
+        self.random_state = random_state
+        self.n_iter = n_iter
+        self.scoring = scoring
+        self.verbose = verbose
+        self.model = BayesSearchCV(
+            RandomForestClassifier(), 
+            search_spaces=self.params, 
+            cv=self.cv, 
+            n_iter=self.n_iter, 
+            scoring=self.scoring, 
+            random_state=self.random_state,
+            verbose=self.verbose
+        )
+
+    def fit(self, train, target) -> None:
+        self.model.fit(train, target)
+ 
+    def predict(self, test):
+        return self.model.predict(test)
+
+
+class XGBClassifierCV(ClassifierModel):
+    def __init__(self, cv: int = 5, random_state: int = RANDOM_STATE, n_iter: int = 25, scoring: str = 'accuracy', verbose: bool = True):
+        self.params = {
+            'learning_rate': Real(0.01, 1.0, prior='log-uniform'),
+            'n_estimators': Integer(50, 1000),
+            'max_depth': Integer(3, 10),
+            'subsample': Real(0.5, 1.0),
+            'colsample_bytree': Real(0.5, 1.0)
+        }
+        self.cv = cv
+        self.random_state = random_state
+        self.n_iter = n_iter
+        self.scoring = scoring
+        self.verbose = verbose
+        self.model = BayesSearchCV(
+            XGBClassifier(), 
+            search_spaces=self.params, 
+            cv=self.cv, 
+            n_iter=self.n_iter, 
+            scoring=self.scoring, 
+            random_state=self.random_state,
+            verbose=self.verbose
+        )
+
+    def fit(self, train, target) -> None:
+        self.model.fit(train, target)
+ 
+    def predict(self, test):
+        return self.model.predict(test)
 
 
 class WFTrainTestGenerator():
